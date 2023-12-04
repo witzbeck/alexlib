a3af104ffa8e76b31f9151452812fa1e12b2deb1
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2023-12-03 20:11:55 -0500

    Update core.py

--- alexlib/core.py
+++ alexlib/core.py
@@ -1,49 +1,17 @@
-from datetime import datetime as dt, timedelta as td
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from hashlib import sha256
+from itertools import chain
+from os import getenv
+from pathlib import Path
+from typing import Any
 from random import randint
+from subprocess import check_output
 
-
-class Object:
-    @property
-    def reg_attrs(self):
-        return {
-            k: v
-            for k, v in self.__dict__.items()
-            if k[0] != "_"
-        }
-
-    @property
-    def reg_attrs_keys(self):
-        return list(self.reg_attrs.keys())
-
-    def set_hasattr(self, attr: str):
-        def hasattr_func(
-                self,
-                attr: str,
-        ) -> bool:
-            if not hasattr(self, attr):
-                ret = False
-            else:
-                ret = getattr(self, attr) is not None
-            return ret
-        newattr = f"has{attr}"
-        if not hasattr(self, newattr):
-            setattr(self, newattr, property(hasattr_func))
-
-    def set_hasattrs(self):
-        for k in self.reg_attrs_keys:
-            if not k.startswith("has"):
-                self.set_hasattr(k)
-
-
-def invert_dict(_dict: dict):
-    """flips the keys and values of a dictionary"""
-    rng = range(len(_dict))
-    vals = list(_dict.values())
-    return {vals[i]: _dict[vals[i]] for i in rng}
-
-
+""" core functions & objects must only use the standard lib
+"""
 # reference datetime for difference calculation
-epoch = dt(
+epoch = datetime(
     year=2,
     month=1,
     day=1,
@@ -56,11 +24,181 @@ epoch = dt(
 # reference datetime as seconds
 epoch_s = epoch.timestamp()
 
-# helper function for states
-istrue = lambda x: x is True
+
+def chktext(
+    text: str,
+    prefix: str = None,
+    value: str = None,
+    suffix: str = None,
+) -> bool:
+    text = text.lower()
+    if prefix:
+        ret = text.startswith(prefix.lower())
+    elif value:
+        ret = value.lower() in text
+    elif suffix:
+        ret = text.endswith(suffix.lower())
+    else:
+        raise ValueError("need valid input")
+    return ret
 
 
-def mk_delta(td_obj: td):
+def chktype(
+        obj: object,
+        type_: type,
+        mustexist: bool = True,
+) -> object:
+    """confirms correct type or raises error"""
+    if not isinstance(obj, type_):
+        raise TypeError(f"input is {type(obj)}, not {type_}")
+    else:
+        ret = obj
+
+    if (ispath := isinstance(obj, Path)):
+        exists = obj.exists()
+    else:
+        exists = True
+
+    if (ispath and mustexist and not exists):
+        raise FileExistsError(f"{obj} must exist but doesn't")
+    else:
+        return ret
+
+
+def envcast(
+    val: str,
+    astype: Any,
+    spliton: str = ",",
+    need: bool = True,
+) -> Any:
+    """converts output to specified type"""
+    if isinstance(astype, str):
+        astype = eval(astype)
+    if issubclass(astype, list):
+        ret = val.split(spliton)
+    elif issubclass(astype, datetime):
+        ret = datetime.fromisoformat(val)
+    else:
+        ret = astype(val)
+    return chktype(ret, astype, mustexist=need)
+
+
+def chkenv(
+    envname: str,
+    need: bool = True,
+    ifnull: bool = None,
+    astype: Any = None,
+) -> Any:
+    """gets/checks/converts environment variable"""
+    val = getenv(envname)
+    isblank = val == ""
+    isnone = val is None
+    istrue = val == "True"
+    isfalse = val == "False"
+    ifnotnone = ifnull is not None
+    astypenotnone = astype is not None
+
+    if ((isblank or isnone) and ifnotnone):
+        return ifnull
+    elif (isnone and need):
+        raise ValueError(envname)
+    elif (isblank and need):
+        raise ValueError(envname)
+    elif (isblank or isnone):
+        return None
+    elif astypenotnone:
+        ret = envcast(val, astype, need=need)
+    elif istrue:
+        ret = True
+    elif isfalse:
+        ret = False
+    else:
+        ret = val
+    return ret
+
+
+def concat_lists(lists: list[list[Any]]) -> list[Any]:
+    return list(chain.from_iterable(
+        lists
+    ))
+
+
+def isdunder(key: str) -> bool:
+    return (key.endswith("__") and key.startswith("__"))
+
+
+def ishidden(key: str) -> bool:
+    return (key.startswith("_") and not isdunder(key))
+
+
+def asdict(
+        obj: object,
+        include_hidden: bool = False,
+        include_dunder: bool = False,
+) -> dict[str: Any]:
+    attrs = list(vars(obj).keys())
+    if not include_dunder:
+        attrs = [x for x in attrs if not isdunder(x)]
+    if not include_hidden:
+        attrs = [x for x in attrs if not ishidden(x)]
+    return {
+        x: getattr(obj, x)
+        for x in attrs
+    }
+
+
+def get_objects_by_attr(
+    lst: list[object],
+    attr: str,
+    val: str,
+) -> list[object]:
+    """filters list of objects based on value of attribute"""
+    return [x for x in lst if getattr(x, attr) == val]
+
+
+class Object(object):
+    @property
+    def reg_attrs(self):
+        return {k: v for k, v in self.__dict__.items() if k[0] != "_"}
+
+    @property
+    def reg_attrs_keys(self):
+        return list(self.reg_attrs.keys())
+
+    def set_hasattr(self, attr: str):
+        def hasattr_func(
+            self,
+            attr: str,
+        ) -> bool:
+            if not hasattr(self, attr):
+                ret = False
+            else:
+                ret = getattr(self, attr) is not None
+            return ret
+
+        newattr = f"has{attr}"
+        if not hasattr(self, newattr):
+            setattr(self, newattr, property(hasattr_func))
+
+    def set_hasattrs(self):
+        for k in self.reg_attrs_keys:
+            if not k.startswith("has"):
+                self.set_hasattr(k)
+
+
+def mk_dictvals_distinct(_dict: dict[Any:Any]) -> dict[Any:Any]:
+    keys = list(_dict.keys())
+    return {key: list(set(_dict[key])) for key in keys}
+
+
+def invert_dict(_dict: dict):
+    """flips the keys and values of a dictionary"""
+    rng = range(len(_dict))
+    vals = list(_dict.values())
+    return {vals[i]: _dict[vals[i]] for i in rng}
+
+
+def mk_delta(td_obj: timedelta) -> timedelta:
     """generates a random timedelta"""
     return td_obj(
         years=randint(0, 100),
@@ -73,10 +211,11 @@ def mk_delta(td_obj: td):
     )
 
 
-class timedelta(td):
+class timedelta(timedelta):
     """new timedelta class with extra methods"""
+
     @classmethod
-    def as_rand(cls) -> td:
+    def as_rand(cls) -> timedelta:
         return mk_delta(cls)
 
     @property
@@ -102,25 +241,25 @@ class timedelta(td):
         # unit of time in a
         # dt datetime or
         # td timedelta
-        print(dir(td(days=1)))
+        print(dir(timedelta(days=1)))
 
         pass
 
     @staticmethod
-    def get_td_s(td: td) -> float:
+    def get_td_s(td: timedelta) -> float:
         return td.total_seconds()
 
-    def get_epoch_self_divmod(self, td_s: td):
+    def get_epoch_self_divmod(self, td_s: timedelta):
         return divmod(self.epoch_self_dif, timedelta.get_td_s(td_s))
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
     def __round__(
-            self,
-            td: td,
-            epoch_s: dt = epoch_s,
-    ) -> td:
+        self,
+        td: timedelta,
+        epoch_s: datetime = epoch_s,
+    ) -> timedelta:
         """
         allows for rounding timedelta to a timedelta
             returns rounded dateti
@@ -133,7 +272,7 @@ class timedelta(td):
         return self.fromtimestamp(epoch_s + dif - mod)
 
 
-def mk_date(dt_obj: dt):
+def mk_date(dt_obj: datetime) -> datetime:
     """generates a random datetime"""
     return dt_obj(
         year=randint(2, 3000),
@@ -146,22 +285,23 @@ def mk_date(dt_obj: dt):
     )
 
 
-class datetime(dt):
+class datetime(datetime):
     """new datetime class with extra methods"""
+
     @classmethod
-    def as_rand(cls) -> dt:
+    def as_rand(cls) -> datetime:
         return mk_date(cls)
 
     @property
-    def epoch(self):
+    def epoch(self) -> datetime:
         return epoch
 
     @property
-    def epoch_s(self):
+    def epoch_s(self) -> int:
         return epoch_s
 
     @property
-    def self_s(self):
+    def self_s(self) -> int:
         return self.timestamp()
 
     @property
@@ -169,20 +309,20 @@ class datetime(dt):
         return self.self_s - self.epoch_s
 
     @staticmethod
-    def get_td_s(td: td) -> float:
+    def get_td_s(td: timedelta) -> float:
         return td.total_seconds()
 
-    def get_epoch_self_divmod(self, td_s: td):
+    def get_epoch_self_divmod(self, td_s: timedelta) -> tuple[float, float]:
         return divmod(self.epoch_self_dif, datetime.get_td_s(td_s))
 
     def __init__(self):
         super().__init__()
 
     def __round__(
-            self,
-            td: td,
-            epoch_s: dt = epoch_s,
-    ) -> dt:
+        self,
+        td: timedelta,
+        epoch_s: datetime = epoch_s,
+    ) -> datetime:
         """
         allows for rounding datetime to a timedel
             returns rounded dateti
@@ -197,3 +337,62 @@ class datetime(dt):
         td_s = datetime.get_td_s(td)
         mod = dif % td_s
         return self.fromtimestamp(epoch_s + dif - mod)
+
+
+def sha256sum(
+    path: Path,
+    bytearr: bytearray = bytearray(128 * 1024),
+) -> str:
+    """ inputs:
+            filename = path + name of file to hash
+        returns:
+            hash of file
+    """
+    if not isinstance(path, Path):
+        raise TypeError("func only computes sum on path")
+    h, mv = sha256(), memoryview(bytearr)
+    with open(path, 'rb', buffering=0) as f:
+        for n in iter(lambda: f.readinto(mv), 0):
+            h.update(mv[:n])
+    return h.hexdigest()
+
+
+def chkhash(path: Path, stored_hash: str) -> bool:
+    return sha256sum(path) == stored_hash
+
+
+def get_last_tag() -> str:
+    return check_output(["git", "describe", "--tags"]).decode("ascii")
+
+
+def get_curent_version(tag: str) -> str:
+    if tag.startswith("v"):
+        tag = tag[1:]
+    if "-" in tag:
+        tag = tag[:tag.index("-")]
+    return tag
+
+
+@dataclass
+class Version:
+    major: int
+    minor: int
+    patch: int
+
+    @classmethod
+    def from_tag(cls):
+        parts = get_curent_version(get_last_tag()).split(".")
+        return cls(*parts)
+
+    def __iter__(self):
+        return iter([
+            self.major,
+            self.minor,
+            self.patch,
+        ])
+
+    def __str__(self) -> str:
+        return ".".join(list(self))
+
+    def __repr__(self) -> str:
+        return str(self)
