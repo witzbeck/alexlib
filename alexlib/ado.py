71fe7bb144e8b7e0fc4f246c7149905d408a490a
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-01-07 15:50:49 -0500

    exe queries

--- alexlib/ado.py
+++ alexlib/ado.py
@@ -3,10 +3,10 @@ from datetime import datetime, timezone
 from functools import cached_property
 from alexlib.__init__ import dotenv
 
-from requests import get
+from requests import Response, get
 from requests.auth import HTTPBasicAuth
 
-from alexlib.core import show_dict, chkenv, get_local_tz
+from alexlib.core import chkenv, get_local_tz
 
 dotenv
 
@@ -16,7 +16,8 @@ class AdoAuth:
     host: str
     org: str
     project: str
-    token: str
+    token: str = field(repr=False)
+    api_version: str = field(default="7.0", repr=False)
 
     @property
     def basic(self) -> HTTPBasicAuth:
@@ -31,9 +32,68 @@ class AdoAuth:
             "project",
             "token",
         ],
+        kwenvs: list[str] = [
+            "api_version",
+        ],
     ):
         envs = [chkenv(f"ado_{x}".upper()) for x in envs]
-        return cls(*envs)
+        kwenvs = {x: chkenv(f"ado_{x}".upper()) for x in kwenvs}
+        return cls(*envs, **kwenvs)
+
+    @property
+    def api_part(self) -> str:
+        return f"?api-version={self.api_version}"
+
+    @property
+    def api_path(self) -> str:
+        return "/".join([
+            self.host,
+            self.org,
+            self.project,
+            "_apis",
+        ])
+
+    def mk_url(
+        self,
+        resource_path: str,
+        kwparts: list[str] = [],
+    ) -> str:
+        return "/".join([
+            self.api_path,
+            resource_path,
+        ]) + "".join(kwparts)
+
+    @property
+    def last_sprint_query_id(self) -> str:
+        return chkenv("ADO_LAST_SPRINT_QUERY_ID")
+
+    @property
+    def last_sprint_url(self) -> str:
+        return self.mk_url(
+            f"wit/wiql/{self.last_sprint_query_id}",
+            kwparts=[self.api_part],
+        )
+
+    def get_response(self, url: str) -> Response:
+        resp = get(url, auth=self.basic)
+        if resp.status_code == 200:
+            return resp
+        else:
+            msg = ", ".join([
+                f"code={resp.status_code}",
+                f"url={resp.url}"
+            ])
+            raise ConnectionError(msg)
+
+    @property
+    def last_sprint_workitems(self) -> list[str]:
+        resp = self.get_response(self.last_sprint_url)
+        return [x["id"] for x in resp.json()["workItems"]]
+
+    @property
+    def workitems(self) -> dict:
+        url = self.mk_url(self.workitem_path, kwparts=[self.api_part])
+        return self.get_response(url)
 
     @property
     def org_initials(self) -> str:
@@ -43,9 +103,14 @@ class AdoAuth:
 @dataclass
 class ProductBacklogItem:
     id: int
-    auth: AdoAuth = field(default_factory=AdoAuth.from_envs)
-    api_version: str = field(default="7.0")
-    resource_path: str = field(default="_apis/wit/workitems")
+    auth: AdoAuth = field(
+        default_factory=AdoAuth.from_envs,
+        repr=False,
+    )
+
+    @property
+    def resource_path(self) -> str:
+        return self.auth.workitem_path
 
     @cached_property
     def tz(self) -> timezone:
@@ -56,18 +121,12 @@ class ProductBacklogItem:
         return datetime.now(tz=self.tz)
 
     @property
-    def api_part(self) -> str:
-        return f"?api-version={self.api_version}"
-
-    @property
     def url(self) -> str:
         return "/".join([
-            self.auth.host,
-            self.auth.org,
-            self.auth.project,
-            self.resource_path,
+            self.auth.api_path,
+            "wit/workitems",
             str(self.id),
-        ]) + self.api_part
+        ]) + self.auth.api_part
 
     @classmethod
     def from_env(cls, env: str = "ADO_PBI_ID", **kwargs):
@@ -208,7 +267,23 @@ class ProductBacklogItem:
     def changed_by_email(self) -> str:
         return self.changed_by["uniqueName"]
 
+    @property
+    def remaining_work(self) -> float:
+        return float(self.fields["Microsoft.VSTS.Scheduling.RemainingWork"])
+
+    @property
+    def original_estimate(self) -> float:
+        return float(self.fields["Microsoft.VSTS.Scheduling.OriginalEstimate"])
+
+    @property
+    def completed_work(self) -> float:
+        return float(self.fields["Microsoft.VSTS.Scheduling.CompletedWork"])
+
 
 if __name__ == "__main__":
     pbi = ProductBacklogItem.from_env()
-    show_dict(pbi.fields)
+    auth = AdoAuth.from_envs()
+    pbis = [
+        ProductBacklogItem(x, auth=pbi.auth)
+        for x in pbi.auth.last_sprint_workitems
+    ]
