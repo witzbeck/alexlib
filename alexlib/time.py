b90f6f291891cbdc3e8197ef744ee35175b75d3d
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-01-14 09:57:20 -0500

    Add .flake8 and .github/workflows/main.yaml files, update auth.py and time.py

--- alexlib/time.py
+++ alexlib/time.py
@@ -1,21 +1,20 @@
 from datetime import datetime, timedelta
+from logging import info
 from decorator import decorator
 from functools import cached_property
 from random import randint
 from time import perf_counter
 from typing import Callable
 
+from pandas import Timestamp
+from pandas.tseries.offsets import BDay
 from pandas.tseries.holiday import USFederalHolidayCalendar, Holiday
 
 from alexlib.constants import epoch_seconds
 
-difthresh = 0.09
-roundto = 6
-
-UNITS = ["s", "ms", "μs", "ns"]
-
 
 def get_rand_datetime() -> datetime:
+    """Generate a random datetime object."""
     return datetime(
         randint(2, 3000),
         randint(1, 12),
@@ -28,6 +27,7 @@ def get_rand_datetime() -> datetime:
 
 
 def get_rand_timedelta() -> timedelta:
+    """Generate a random timedelta object."""
     return timedelta(
         weeks=randint(0, 100),
         days=randint(0, 30),
@@ -39,7 +39,7 @@ def get_rand_timedelta() -> timedelta:
 
 
 class CustomTimedelta(timedelta):
-    """new timedelta class with extra methods"""
+    """custom timedelta class with extra methods"""
 
     @classmethod
     def rand(cls) -> timedelta():
@@ -65,20 +65,14 @@ class CustomTimedelta(timedelta):
     def get_epoch_self_divmod(self, td: timedelta) -> tuple[float, float]:
         return divmod(self.epoch_self_dif, td.total_seconds())
 
-    def __round__(self, td: timedelta) -> timedelta:
-        """
-        allows for rounding timedelta to a timedelta
-            returns rounded dateti
-            1. converts both to seconds
-            2. calcs difference between both and epoch
-        """
-        dif = self.epoch_self_dif
-        mod = dif % td.total_seconds()
-        return self.fromtimestamp(epoch_seconds + dif - mod)
+    def __round__(self, other: timedelta) -> timedelta:
+        diff = self.epoch_self_diff
+        mod = diff % other.total_seconds()
+        return self.__class__(seconds=diff - mod)
 
 
 class CustomDatetime(datetime):
-    """new datetime class with extra methods"""
+    """custom datetime class with extra methods"""
 
     @classmethod
     def rand(cls) -> datetime:
@@ -90,8 +84,7 @@ class CustomDatetime(datetime):
 
     @property
     def isholiday(self) -> bool:
-        dt = datetime(self.year, self.month, self.day)
-        return dt in self.holidays
+        return self.date() in self.holidays
 
     @property
     def isweekday(self) -> bool:
@@ -113,11 +106,15 @@ class CustomDatetime(datetime):
     def tomorrow(self) -> datetime:
         return self + timedelta(days=1)
 
+    @cached_property
+    def one_busday(self) -> BDay:
+        return BDay(1)
+
     def get_last_busday(self) -> datetime:
-        chkdate = self.yesterday
-        while not chkdate.isbusinessday:
-            chkdate = chkdate.yesterday
-        return chkdate
+        chkdate = Timestamp(self.yesterday)
+        while chkdate in self.holidays:
+            chkdate = chkdate - self.one_busday
+        return chkdate.to_pydatetime()
 
     @property
     def epoch_self_dif(self) -> float:
@@ -127,47 +124,27 @@ class CustomDatetime(datetime):
         return divmod(self.epoch_self_dif, td.total_seconds())
 
     def __round__(self, td: timedelta) -> datetime:
-        """
-        allows for rounding datetime to a timedelta
-            returns rounded datetime
-            1. converts both datetime and timedelta to seconds
-            2. calcs difference between datetime and epoch
-        self_s, td_s = self.timestamp(), td.total_seconds()
-        dif = self_s - epoch_s      # dif = difference in seconds
-        mod = dif % td_s            # mod = modulus of dif and td_s
-        return self.fromtimestamp(epoch_s + (dif - mod))
-        """
-        dif = self.epoch_self_dif
-        mod = dif % td.total_seconds()
-        return self.fromtimestamp(epoch_seconds + dif - mod)
+        mod = self.epoch_self_dif % td.total_seconds()
+        return self.fromtimestamp(epoch_seconds + self.epoch_self_dif - mod)
 
 
 @decorator
-def timeit(
-    func: Callable,
-    niter: int = None,
-    *args,
-    **kwargs,
-) -> None:
+def timeit(func: Callable, niter: int = None, *args, **kwargs) -> None:
+    unit_index, threshold, roundto = 0, 0.09, 6
+    UNITS = ["s", "ms", "μs", "ns"]
     pc1 = perf_counter()
-    if niter is None:
-        ret = func(*args, **kwargs)
-    else:
-        ret = [func() for _ in range(niter)]
-    i, dif = 0, perf_counter() - pc1
-    while dif <= difthresh:
-        i += 1
+    result = func(*args, **kwargs) if niter is None else [func() for _ in range(niter)]
+    dif = perf_counter() - pc1
+    while dif <= threshold:
+        unit_index += 1
         dif *= 10e3
     loopstr = f" in {niter} loops" if niter is not None else ""
-    res = f"{str(round(dif, roundto))} {UNITS[i]}"
+    res = f"{str(round(dif, roundto))} {UNITS[unit_index]}"
     msg = f"{func.__name__} took {res}{loopstr}"
     try:
-        if niter is None:
-            nres = len(ret)
-        else:
-            nres = sum([len(x) for x in ret])
+        nres = len(result) if niter is None else sum([len(x) for x in result])
         msg = f"{msg} and returned {nres} results"
     except TypeError:
-        pass
+        info("timeit: result is not iterable")
     print(msg)
-    return ret
+    return result
