e4687e1a17f2da3c33347ae6f0c5e94da3e37832
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-01-20 16:10:33 -0500

    Refactor GPT classes and add docstrings

--- alexlib/ml/gpt.py
+++ alexlib/ml/gpt.py
@@ -1,21 +1,17 @@
+"""GPT classes"""
 from dataclasses import dataclass, field
-import logging as log
 
+# from openai import OpenAI
 from pandas import DataFrame
 
 from alexlib.core import chkenv
-from alexlib.config import DotEnv
-from alexlib.db import Table, Connection
-
-if __name__ == "__main__":
-    schema = "gpt"
-    config = DotEnv()
-    c = Connection.from_context("LOCAL")
-    log.info(schema)
+from alexlib.db import Connection, Table
 
 
 @dataclass
 class Message:
+    """A message"""
+
     message_id: int
     message_seq: int
     role: str
@@ -26,15 +22,19 @@ class Message:
 
     @property
     def record(self) -> dict:
+        """Returns a dict of the message"""
         return {x: getattr(self, x) for x in self.__dict__ if x[0] != "_"}
 
     @classmethod
-    def from_dict(cls, d: dict):
+    def from_dict(cls, d: dict) -> "Message":
+        """Creates a Message from a dict"""
         return cls(**d)
 
 
 @dataclass
 class Messages:
+    """A list of messages"""
+
     lst: list[Message] = field(default_factory=list)
     context: str = "LOCAL"
     schema: str = "gpt"
@@ -43,33 +43,41 @@ class Messages:
 
     @property
     def nmsgs(self) -> int:
+        """Returns the number of messages"""
         return len(self.lst)
 
     @property
     def rng(self) -> range:
+        """Returns a range of message ids"""
         return range(self.nmsgs)
 
     def update_attr(self, attr: str, vals: list) -> None:
-        [setattr(msg, attr, vals[i]) for i, msg in enumerate(self.lst)]
+        """Updates an attribute of the messages"""
+        return [setattr(msg, attr, vals[i]) for i, msg in enumerate(self.lst)]
 
     def get_update_ids_vals(self, last_id: int) -> list[str]:
+        """Returns a list of message ids"""
         return [i + last_id + 1 for i in range(len(self.lst))]
 
-    def update_ids(self, last_id: int):
+    def update_ids(self, last_id: int) -> "Messages":
+        """Updates the message ids"""
         ids = self.get_update_ids_vals(last_id)
         self.update_attr("message_id", ids)
         return self
 
     @property
     def record_list(self) -> list[dict]:
+        """Returns a list of dicts of the messages"""
         return [x.record for x in self.lst]
 
     @property
     def df(self) -> DataFrame:
+        """Returns a DataFrame of the messages"""
         return DataFrame.from_records([x.record for x in self.lst])
 
     @property
     def tbl(self) -> Table:
+        """Returns the table object"""
         return Table.from_db(self.context, self.schema, self.table)
 
     @classmethod
@@ -79,7 +87,9 @@ class Messages:
         is_input: bool = False,
         is_return: bool = False,
         spiciness: float = -1.0,
-    ):
+    ) -> "Messages":
+        """Creates a Messages object from a list of dicts"""
+
         def msg_from_dict(d: dict, i: int):
             d["message_id"] = 0
             d["message_seq"] = i
@@ -99,7 +109,8 @@ class Messages:
         df: DataFrame,
         is_input: bool = False,
         is_return: bool = False,
-    ):
+    ) -> "Messages":
+        """Creates a Messages object from a DataFrame"""
         lst = df.to_dict(orient="records")
         return cls.from_list(
             lst,
@@ -110,22 +121,23 @@ class Messages:
     @classmethod
     def from_db(
         cls,
-        context: str = "LOCAL",
+        cnxn: Connection,
         schema: str = "gpt",
         table: str = "messages",
-    ):
-        tbl = Table.from_db(context, schema, table)
-        df = tbl.get_df()
-        return cls.from_df(df)
+    ) -> "Messages":
+        """Creates a Messages object from a database table"""
+        tbl = Table.from_db(cnxn, schema, table)
+        return cls.from_df(tbl.df)
 
 
 @dataclass
 class Completion:
+    """A completion"""
+
     id: int
     obj: object
     temperature: float
     return_message: Message
-    """
     max_tokens: int
     top_p: float
     frequency_penalty: float
@@ -147,18 +159,14 @@ class Completion:
     engine: str
     max_rerank: int
     n_best_size: int
-    """
 
 
 @dataclass
 class Experiment:
+    """An experiment""" ""
+
     id: int
     model: str = chkenv("OPENAI_MODEL")
     endpoint: str = chkenv("OPENAI_API_ENDPOINT")
     input_messages: list[Message] = field(default_factory=list)
     completions: list[Message] = field(default_factory=list)
-
-
-if __name__ == "__main__":
-    df = c.get_table("gpt", "messages")
-    print(df)
